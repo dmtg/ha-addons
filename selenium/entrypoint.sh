a8e64002b3186b93c91f6ec500774a032e467cb3
dmtg <deliogois@gmail.com>
2025-04-07 00:10:52 +0100

    Delete selenium directory

--- selenium/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-source stop_trap.sh
-
-java -jar $SELENIUM_JAR \
-	-port $SELENIUM_HUB_PORT \
-	&
-JAVA_PID=$!
-wait $JAVA_PID
--- selenium/stop_trap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-stop_trap() {
-	kill -SIGTERM $JAVA_PID
-	wait $JAVA_PID
-	exit 0
-}
-
-trap stop_trap SIGINT SIGTERM
