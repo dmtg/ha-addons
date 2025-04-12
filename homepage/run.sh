08cc616b85f09d8e95db8885065f45c6e118d3c0
dmtg <deliogois@gmail.com>
2025-04-12 13:34:44 +0100

    repor

--- homepage/run.sh
+++ homepage/run.sh
@@ -1,21 +1,48 @@
-#!/bin/bash
+#!/bin/sh
 
-# Carregar as opções do arquivo options.json
-ALLOWED_HOSTS=$(jq -r '.HOMEPAGE_ALLOWED_HOSTS' /data/options.json)
+# Define paths for Homepage's data
+HOMEPAGE_CONFIG_PATH="/app/config"
 
-# Se não for definida, usar o valor padrão "*"
-ALLOWED_HOSTS="${ALLOWED_HOSTS:-*}"
+# Mapped directories from the host
+PERSISTENT_CONFIG_PATH="/share/homepage/configs"
 
-# Verifica se o hostname foi corretamente passado
-HOSTNAME=${HOSTNAME:-"localhost"}  # Se não houver HOSTNAME, define um valor padrão
+# Ensure the persistent directories exist
+mkdir -p $PERSISTENT_CONFIG_PATH
 
-# Imprimir as variáveis para verificação
-echo "Allowed hosts are set to: ${ALLOWED_HOSTS}"
-echo "Using hostname: ${HOSTNAME}"
+# Function to sync data from Homepage's directories to persistent storage
 
-# Exemplo de usar a variável em outro comando (por exemplo, no node server)
-# node /app/server.js --allowed-hosts="${ALLOWED_HOSTS}"
 
-# Ou outro comando necessário
-exec "$@"
 
+sync_to_persistent() {
+    while true; do
+        cp -R $HOMEPAGE_CONFIG_PATH/* $PERSISTENT_CONFIG_PATH/ 2>/dev/null
+        sleep 60  # Sync every 60 seconds, adjust as needed
+    done
+}
+
+# Sync data to Homepage's directories on startup
+cp -R $PERSISTENT_CONFIG_PATH/* $HOMEPAGE_CONFIG_PATH/ 2>/dev/null
+
+# Start continuous sync in the background
+sync_to_persistent &
+
+# Exporting hostname
+echo "Exporting hostname..."
+export NEXTAUTH_URL_INTERNAL="http://$HOSTNAME:${PORT:-7575}"
+
+# Migrating database
+#echo "Migrating database..."
+#cd ./migrate; yarn db:migrate & PID=$!
+
+# Wait for migration to finish
+#wait $PID
+
+# Check and copy default.json if necessary
+#cp -n /app/config/default.json /app/data/config/default.json
+
+# Starting Homepage
+echo "Starting production server..."
+node /app/server.js & PID=$!
+
+# Wait for Homarr server process to end
+wait $PID
