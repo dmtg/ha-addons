889e3de9e982b7449592d7dbb0cb2cf51e110a5d
dmtg <deliogois@gmail.com>
2025-04-12 12:59:08 +0100

    Update run.sh

--- homepage/run.sh
+++ homepage/run.sh
@@ -1,45 +1,37 @@
 #!/bin/sh
 
+# Recolhe variáveis do ficheiro /data/options.json
+PORT=$(jq -r '.port' /data/options.json)
+HOSTNAME=$(jq -r '.custom_hostname' /data/options.json)
+ALLOWED_HOSTS=$(jq -r '.HOMEPAGE_ALLOWED_HOSTS' /data/options.json)
+
 # Define paths for Homepage's data
 HOMEPAGE_CONFIG_PATH="/app/config"
-
-# Mapped directories from the host
 PERSISTENT_CONFIG_PATH="/share/homepage/configs"
 
-# Ensure the persistent directories exist
+# Garante que o diretório existe
 mkdir -p $PERSISTENT_CONFIG_PATH
 
-# Function to sync data from Homepage's directories to persistent storage
+# Copia configs persistentes ao iniciar
+cp -R $PERSISTENT_CONFIG_PATH/* $HOMEPAGE_CONFIG_PATH/ 2>/dev/null
+
+# Sincronização em background
 sync_to_persistent() {
     while true; do
         cp -R $HOMEPAGE_CONFIG_PATH/* $PERSISTENT_CONFIG_PATH/ 2>/dev/null
-        sleep 60  # Sync every 60 seconds, adjust as needed
+        sleep 60
     done
 }
-
-# Sync data to Homepage's directories on startup
-cp -R $PERSISTENT_CONFIG_PATH/* $HOMEPAGE_CONFIG_PATH/ 2>/dev/null
-
-# Start continuous sync in the background
 sync_to_persistent &
 
-# Exporting hostname
+# Exporta as variáveis de ambiente
 echo "Exporting hostname..."
-export NEXTAUTH_URL_INTERNAL="http://$HOSTNAME:${PORT:-7575}"
+export NEXTAUTH_URL_INTERNAL="http://$HOSTNAME:$PORT"
+export HOMEPAGE_ALLOWED_HOSTS="$ALLOWED_HOSTS"
 
-# Migrating database
-#echo "Migrating database..."
-#cd ./migrate; yarn db:migrate & PID=$!
-
-# Wait for migration to finish
-#wait $PID
-
-# Check and copy default.json if necessary
-#cp -n /app/config/default.json /app/data/config/default.json
-
-# Starting Homepage
+# Inicia o servidor
 echo "Starting production server..."
 node /app/server.js & PID=$!
 
-# Wait for Homarr server process to end
+# Espera pelo processo
 wait $PID
