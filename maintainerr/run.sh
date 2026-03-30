b2cc4b5e8dcf10c132fae292e9e730873d04281e
dmtg <deliogois@gmail.com>
2026-03-30 01:56:32 +0100

    Update run.sh

--- maintainerr/run.sh
+++ maintainerr/run.sh
@@ -3,11 +3,11 @@
 echo "A iniciar Maintainerr com persistência..."
 
 # garantir pasta persistente
-mkdir -p /share/maintainerr
+mkdir -p /addon_configs/maintainerr
 
 # ligar pasta ao caminho usado pela app (ajustar se necessário)
 rm -rf /opt/data
-ln -s /share/maintainerr /opt/data
+ln -s /addon_configs/maintainerr /opt/data
 
 # arrancar processo original
 exec /init
