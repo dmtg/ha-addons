fa35b5beb2b1fc5045106167e5ce80e3383256e3
dmtg <deliogois@gmail.com>
2026-03-30 01:22:13 +0100

    Update run.sh

--- maintainerr/run.sh
+++ maintainerr/run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # criar pasta persistente se não existir
-mkdir -p /share/maintainerr
+mkdir -p /addon_configs/maintainerr
 
 # substituir /opt/data por symlink
 rm -rf /opt/data
-ln -s /share/maintainerr /opt/data
+ln -s /addon_configs/maintainerr /opt/data
 
 # arrancar app original
 exec /init
