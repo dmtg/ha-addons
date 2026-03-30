f7924e5aae287267f96db614bef0ddeb88e55417
dmtg <deliogois@gmail.com>
2026-03-30 02:10:02 +0100

    Update run.sh

--- maintainerr/run.sh
+++ maintainerr/run.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-echo "A iniciar Maintainerr..."
+echo "A iniciar Maintainerr com persistência..."
 
 PERSISTENT_PATH="/addon_configs/maintainerr"
-APP_PATH="/opt/data"   # confirmar depois se necessário
+APP_PATH="/config"
 
+# garantir pasta persistente
 mkdir -p $PERSISTENT_PATH
 
+# substituir /config por symlink
 rm -rf $APP_PATH
 ln -s $PERSISTENT_PATH $APP_PATH
 
+# arrancar aplicação
 exec /init
