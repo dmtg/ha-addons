907568f4ae436cfb89af38e8e24bbd220dba3ec4
dmtg <deliogois@gmail.com>
2026-03-30 13:06:24 +0100

    Update run.sh

--- maintainerr/run.sh
+++ maintainerr/run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/with-contenv sh
 
 mkdir -p /opt/data
-echo "Exporting hostname..."
 export NEXTAUTH_URL_INTERNAL="http://$HOSTNAME:${PORT:-6246}"
-echo "Starting production server..."
+
 exec /init
