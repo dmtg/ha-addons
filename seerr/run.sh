ccf6f1154009e99909b5488498d766105793bdff
dmtg <deliogois@gmail.com>
2026-04-20 17:47:14 +0100

    Update run.sh

--- seerr/run.sh
+++ seerr/run.sh
@@ -3,15 +3,11 @@
 CONFIG_PATH="/share/seerr/configs"
 
 mkdir -p "$CONFIG_PATH"
-
-# Permissões para o user do Seerr (node = 1000)
 chown -R 1000:1000 "$CONFIG_PATH"
-chmod -R 775 "$CONFIG_PATH"
 
-# Substituir config interna
 rm -rf /app/config
 ln -s "$CONFIG_PATH" /app/config
 
-echo "Starting Seerr using config at $CONFIG_PATH"
+echo "Starting Seerr..."
 
 exec /app/seerr
