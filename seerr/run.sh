5aaffdf0f09f65e437bab9bdf63c2e6c68a9fbed
dmtg <deliogois@gmail.com>
2026-04-20 17:54:08 +0100

    Update run.sh

--- seerr/run.sh
+++ seerr/run.sh
@@ -2,20 +2,13 @@
 
 CONFIG_PATH="/share/seerr/configs"
 
-echo "Fixing permissions..."
-
-# correr como root primeiro
-mkdir -p "$CONFIG_PATH"
-chmod -R 777 /share
-
-# criar estrutura esperada pelo Seerr
+# garantir estrutura mínima
 mkdir -p "$CONFIG_PATH/logs"
 
-# limpar config interna e ligar ao share
+# ligar config
 rm -rf /app/config
 ln -s "$CONFIG_PATH" /app/config
 
 echo "Starting Seerr..."
 
-# correr como user node (UID 1000)
-exec su-exec 1000:1000 node dist/index.js
+exec node dist/index.js
